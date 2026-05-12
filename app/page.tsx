21c75294d097abd026dfe1b37cb1d6fd974539c9
Adair Rohling <rohling@users.noreply.github.com>
2026-05-12 01:06:16 +0000

    feat: header e banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-
+import Banner from "./components/Banner";
+import Header from "./components/Header"
 
 export default function Home() {
   return (
-
-    <h1>Teste</h1>
-    
-
+    <div>
+      <Header />
+      <Banner />
+    </div>
   );
 }
